94a90493ee467e40973d4f909616162f1da3f237
Victor Martins <vms5@cin.ufpe.br>
2021-10-04 11:24:55 -0300

    video em pagina propria

--- server.js
+++ server.js
@@ -37,7 +37,18 @@ server.get('/portfolio', function(req, res){
     return res.render('portfolio', { items: videos });
 })
 
+server.get("/video", function(req, res){
+    const id = req.query.id;
 
+    const video = videos.find(function(video){
+        return video.id == id
+    }) 
+
+    if(!video)
+        return res.send("video not found");
+
+    res.render("video", {item: video});
+})
 
 server.listen(5000, function(){
     console.log('server is running');
